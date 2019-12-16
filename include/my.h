02ad0c86a14e1b30caa55fa69fd256dac1eff35b
Quentin TREHEUX <qtreheux@localhost.localdomain>
2019-12-16 11:58:33 +0100

    [ADD] makefile / include / src / main.c / my.h

---
+++ include/my.h
@@ -0,0 +1,17 @@
+
+/*
+** EPITECH PROJECT, 2019
+** my.h
+** File description:
+** 103architect
+*/
+
+#ifndef MY_H_
+#define MY_H_
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#endif
