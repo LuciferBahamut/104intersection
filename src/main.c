d655c075c404bee9328a1164d879574d7d709390
Quentin TREHEUX <qtreheux@localhost.localdomain>
2019-12-21 19:44:00 +0100

    [FIX] problem (corrupted memory) with error in malloc

--- src/main.c
+++ src/main.c
@@ -9,24 +9,16 @@
 
 int main(int ac, char **av)
 {
-    // CORRUPTION DE MEMOIRE !!!
-//    int *list = malloc(ac - 1);
+    int *list = malloc((ac - 1) * sizeof(int));
+
     if (error_handling(ac, av) == 1)
         return(84);
     if (av[1][0] == '-' && av[1][1] == 'h') {
         display_help();
         return (0);
     }
-//    for (int i = 1; i < ac; i++)
-//        list[i - 1] = atoi(av[i]);
-//    printf("%d\n", list[0]);
-//    printf("%d\n", list[1]);
-//    printf("%d\n", list[2]);
-//    printf("%d\n", list[3]);
-//    printf("%d\n", list[4]);
-//    printf("%d\n", list[5]);
-//    printf("%d\n", list[6]);
-//    printf("%d\n", list[7]);
-//    free(list);
+    for (int i = 1; i < ac; i++)
+        list[i - 1] = atoi(av[i]);
+    free(list);
     return (0);
 }
