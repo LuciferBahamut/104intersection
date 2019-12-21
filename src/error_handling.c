ebc0d117724e2c1bd171fc2016b2bcf610372cc3
Quentin TREHEUX <qtreheux@localhost.localdomain>
2019-12-21 19:35:42 +0100

    [UPD] error_handling

--- src/error_handling.c
+++ src/error_handling.c
@@ -7,8 +7,16 @@
 
 int error_handling(int ac, char **av)
 {
-    if (ac == 2 || ac == 9)
+    if (ac == 2 && av[1][0] == '-' && av[1][1] == 'h')
         return (0);
-    else
+    if (ac != 9)
+        return (1);
+    for (int i = 1; i != ac; i++)
+        for (int j = 0; j != 2; j++)
+            if (av[i][j] < '0' && av[i][j] > '9')
+                return (1);
+    if (av[1][0] != '1' && av[1][0] != '2' && av[1][0] != '3')
+        return (1);
+    if (av[8][0] == '-')
         return (1);
 }
