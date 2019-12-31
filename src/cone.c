6aa91ee89f8534a17a29b132765ba4fd7e2090f5
Quentin TREHEUX <qtreheux@localhost.localdomain>
2019-12-31 14:20:15 +0100

    [FIX] error in display [ADD] gest infinite intersection

--- src/cone.c
+++ src/cone.c
@@ -13,8 +13,11 @@ int calc_cone(int *t)
 
     printf("Cone with a %d degree angle\n", t[7]);
     printf("Line passing through the point (%d, %d, %d) ", t[1], t[2], t[3]);
-    printf("and parrallel to the vector (%d, %d, %d)\n", t[4], t[5] , t[6]);
-    compute_des(equ, t);
+    printf("and parallel to the vector (%d, %d, %d)\n", t[4], t[5] , t[6]);
+    if (equ[0] == 0)
+        printf("There is an infinite number of intersection points.\n");
+    else
+        compute_des(equ, t);
     free(equ);
     return (0);
 }
--- src/cylinder.c
+++ src/cylinder.c
@@ -14,7 +14,10 @@ int calc_cylinder(int *t)
     printf("Cylinder of radius %d\n", t[7]);
     printf("Line passing through the point (%d, %d, %d) ", t[1], t[2], t[3]);
     printf("and parallel to the vector (%d, %d, %d)\n", t[4], t[5], t[6]);
-    compute_des(equ, t);
+    if (equ[0] == 0)
+        printf("There is an infinite number of intersection points.\n");
+    else
+        compute_des(equ, t);
     free(equ);
     return (0);
 }
